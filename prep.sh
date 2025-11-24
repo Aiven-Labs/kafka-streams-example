d352af2f26480446a8213146713ad54a7a67fbb0
Tony Ibbs (Tibs) <tony.ibbs@aiven.io>
2025-11-24 10:23:35 +0000

    Use a more sensible origin directory

--- prep.sh
+++ prep.sh
@@ -1,3 +1,3 @@
-export CA_PEM_CONTENTS "$(cat ~/kcat-certs/ca.pem)"
-export SERVICE_CERT_CONTENTS "$(cat ~/kcat-certs/service.cert)"
-export SERVICE_KEY_CONTENTS "$(cat ~/kcat-certs/service.key)"
+export CA_PEM_CONTENTS "$(cat certs/ca.pem)"
+export SERVICE_CERT_CONTENTS "$(cat certs/service.cert)"
+export SERVICE_KEY_CONTENTS "$(cat certs/service.key)"
