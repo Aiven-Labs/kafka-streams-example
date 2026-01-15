41c7a841806bbba965bdbb34c0ba3c285d45b4c3
Tony Ibbs (Tibs) <tony.ibbs@aiven.io>
2026-01-15 15:37:00 +0000

    Basic tests now in place for GenericFilterApp

--- app/src/test/java/org/example/GenericFilterAppTest.java
+++ app/src/test/java/org/example/GenericFilterAppTest.java
@@ -5,7 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.avro.AvroRuntimeException;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.Topology;
@@ -13,10 +18,9 @@ import org.apache.kafka.streams.TopologyTestDriver;
 import org.apache.kafka.streams.TestInputTopic;
 import org.apache.kafka.streams.TestOutputTopic;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
+import org.apache.kafka.streams.errors.LogAndFailProcessingExceptionHandler;
+import org.apache.kafka.streams.errors.StreamsException;
+import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import uk.org.webcompere.systemstubs.jupiter.SystemStub;
@@ -71,6 +75,23 @@ class SetupMockSchemaRepository {
     }
 }
 
+class SetupProperties {
+    /**
+     * Set some standard test values as system properties
+     */
+    static void setProperties(String inputTopicName, String outputTopicName) {
+        System.setProperty("KAFKA_SERVICE_URI", "dummy:1234");
+        System.setProperty("SSL_TRUSTSTORE_LOCATION", "not used");
+        System.setProperty("SSL_KEYSTORE_LOCATION", "not used");
+        System.setProperty("PASSWORD_FOR_STORE", "not used");
+        System.setProperty("SCHEMA_REGISTRY_URL", "mock://test_schema_registry");
+        System.setProperty("SCHEMA_REGISTRY_PASSWORD", "not used");
+        System.setProperty("SCHEMA_REGISTRY_USERNAME", "not used");
+        System.setProperty("INPUT_TOPIC", inputTopicName);        // the default
+        System.setProperty("OUTPUT_TOPIC", outputTopicName);       // the default
+    }
+}
+
 @ExtendWith(SystemStubsExtension.class)
 class GenericFilterAppTests {
 
@@ -78,84 +99,176 @@ class GenericFilterAppTests {
     @SystemStub
     private SystemProperties systemProperties;
 
-    static final String inputTopicName = "logistics_data_gen";
-    static final String outputTopicName = "logistics_data_delivered";
-
     static TopologyTestDriver testDriver;
 
-    static TestInputTopic<String, logistics> inputTopic;
-    static TestOutputTopic<String, logistics_delivered> outputTopic;
+    static Properties config;
 
+    @Nested
+    @DisplayName("When input is logistics data")
+    class InputIsLogisticsData {
 
-    /** Set some standard test values as system properties */
-    static void setProperties()
-    {
-        System.setProperty("KAFKA_SERVICE_URI",         "dummy:1234");
-        System.setProperty("SSL_TRUSTSTORE_LOCATION",   "not used");
-        System.setProperty("SSL_KEYSTORE_LOCATION",     "not used");
-        System.setProperty("PASSWORD_FOR_STORE",        "not used");
-        System.setProperty("SCHEMA_REGISTRY_URL",       "mock://test_schema_registry");
-        System.setProperty("SCHEMA_REGISTRY_PASSWORD",  "not used");
-        System.setProperty("SCHEMA_REGISTRY_USERNAME",  "not used");
-        System.setProperty("INPUT_TOPIC",  inputTopicName);        // the default
-        System.setProperty("OUTPUT_TOPIC", outputTopicName);       // the default
+        static TestInputTopic<String, logistics> inputTopic;
+        static TestOutputTopic<String, logistics_delivered> outputTopic;
+
+        static final String inputTopicName = "logistics_data_gen";
+        static final String outputTopicName = "logistics_data_delivered";
+
+        @BeforeAll
+        static void setup() {
+            SetupProperties.setProperties(inputTopicName, outputTopicName);
+
+            config = Config.getConfig();
+            Map<String, String> serdeConfig = Config.getSerdeConfig(config);
+
+            SetupMockSchemaRepository.registerSchemas();
+
+            Topology topology = GenericFilterApp.buildTopology(config, serdeConfig);
+            testDriver = new TopologyTestDriver(topology, config);
+
+            SpecificAvroSerde<logistics> inputSerde = new SpecificAvroSerde<>();
+            inputSerde.configure(serdeConfig, false);  // Just for the value, not the key
+
+            SpecificAvroSerde<logistics_delivered> outputSerde = new SpecificAvroSerde<>();
+            outputSerde.configure(serdeConfig, false); // just for the value
+
+            inputTopic = testDriver.createInputTopic(inputTopicName, Serdes.String().serializer(), inputSerde.serializer());
+            outputTopic = testDriver.createOutputTopic(outputTopicName, Serdes.String().deserializer(), outputSerde.deserializer());
+        }
+
+        @AfterAll
+        static void tearDown() {
+            testDriver.close();
+        }
+
+        @Test
+        @DisplayName("Test a Delivered message goes through")
+        void testDeliveredMessagePropagates() {
+            var now = System.currentTimeMillis();
+            logistics inputValue = logistics.newBuilder()
+                    .setState("Delivered")
+                    .setTimeUtc(now)
+                    .setTrackingId("TRACK-ABC123")
+                    .setCarrier("UPS")
+                    .setMessage("Hidden somewhere outside your house")
+                    .setManifest(List.of("Cosy jumper", "Wooly hat"))
+                    .setNextHopLocation("None")
+                    .build();
+
+            System.out.println("Input value " + inputValue);
+
+            inputTopic.pipeInput("key", inputValue);
+
+            KeyValue<String, logistics_delivered> outputRecord = outputTopic.readKeyValue();
+
+            assertNotNull(outputRecord);
+            assertEquals("key", outputRecord.key);
+            assertEquals(now, outputRecord.value.getTimeUtc());
+            assertEquals("UPS", outputRecord.value.getCarrier());
+            assertEquals(List.of("Cosy jumper", "Wooly hat"), outputRecord.value.getManifest());
+            assertEquals("TRACK-ABC123", outputRecord.value.getTrackingId());
+        }
+
+        @Test
+        @DisplayName("Test a Processing message does not go through")
+        void testProcessingMessageDoesNotPropagate() {
+            var now = System.currentTimeMillis();
+            logistics inputValue = logistics.newBuilder()
+                    .setState("Processing")
+                    .setTimeUtc(now)
+                    .setTrackingId("TRACK-ABC123")
+                    .setCarrier("UPS")
+                    .setMessage("Hidden somewhere outside your house")
+                    .setManifest(List.of("Cosy jumper", "Wooly hat"))
+                    .setNextHopLocation("None")
+                    .build();
+
+            System.out.println("Input value " + inputValue);
+
+            inputTopic.pipeInput("key", inputValue);
+
+            assertTrue(outputTopic.isEmpty(), "Processing message does not come through");
+        }
     }
 
-    @BeforeAll
-    static void setup() {
-        setProperties();
+    @Nested
+    @DisplayName("When input is partial data")
+    class InputIsPartialData {
 
-        Properties config = Config.getConfig();
-        Map<String, String> serdeConfig = Config.getSerdeConfig(config);
+        static TestInputTopic<String, GenericRecord> inputTopic;
+        static TestOutputTopic<String, logistics_delivered> outputTopic;
 
-        SetupMockSchemaRepository.registerSchemas();
+        static Properties config;
 
-        Topology topology = GenericFilterApp.buildTopology(config, serdeConfig);
-        testDriver = new TopologyTestDriver(topology, config);
+        static final String inputTopicName = "logistics_data_partial";  // not the same topic as the main tests
+        static final String outputTopicName = "logistics_data_delivered";
 
-        SpecificAvroSerde<logistics> inputSerde = new SpecificAvroSerde<>();
-        inputSerde.configure(serdeConfig, false);  // Just for the value, not the key
+        static AvroSchema inputSchema;
 
-        SpecificAvroSerde<logistics_delivered> outputSerde = new SpecificAvroSerde<>();
-        outputSerde.configure(serdeConfig, false); // just for the value
+        @BeforeAll
+        static void setup() {
+            SetupProperties.setProperties(inputTopicName, outputTopicName);
 
-        inputTopic = testDriver.createInputTopic(inputTopicName, Serdes.String().serializer(), inputSerde.serializer());
-        outputTopic = testDriver.createOutputTopic(outputTopicName, Serdes.String().deserializer(), outputSerde.deserializer());
-    }
+            config = Config.getConfig();
+            Map<String, String> serdeConfig = Config.getSerdeConfig(config);
 
-    @AfterAll
-    static void tearDown() {
-        testDriver.close();
-    }
+            SetupMockSchemaRepository.registerSchemas();
 
-    @Test
-    @DisplayName("Test a Delivered message goes through")
-    void testDeliveredMessagePropagares()
-    {
-        var now = System.currentTimeMillis();
-        logistics inputValue = logistics.newBuilder()
-                .setState("Delivered")
-                .setTimeUtc(now)
-                .setTrackingId("TRACK-ABC123")
-                .setCarrier("UPS")
-                .setMessage("Hidden somewhere outside your house")
-                .setManifest(List.of("Cosy jumper", "Wooly hat"))
-                .setNextHopLocation("None")
-                .build();
+            // We want to use a different Avro schema for the sent message,
+            // as none of the fields in the normal logistics_gen schema are optional
+            Schema.Parser parser = new Schema.Parser();
+            inputSchema = new AvroSchema("""
+                    {
+                      "namespace": "data.gen.avro",
+                      "name": "partialLogistics",
+                      "type": "record",
+                      "fields": [ { "name": "state", "type": { "type": "string" } } ]
+                    }
+                    """);
 
-        System.out.println("Input value " + inputValue);
+            SchemaRegistryClient client = MockSchemaRegistry.getClientForScope("test_schema_registry");
+            try {
+                client.register("test_schema_registry", inputSchema);
+            } catch (IOException | RestClientException e) {
+                System.out.println("Unable to register input schema " + e);
+            }
 
-        inputTopic.pipeInput("key", inputValue);
+            Topology topology = GenericFilterApp.buildTopology(config, serdeConfig);
+            testDriver = new TopologyTestDriver(topology, config);
 
-        KeyValue<String, logistics_delivered> outputRecord = outputTopic.readKeyValue();
+            GenericAvroSerde inputSerde = new GenericAvroSerde();
+            inputSerde.configure(serdeConfig, false); // just for the value
 
-        assertNotNull(outputRecord);
-        assertEquals("key", outputRecord.key);
-        assertEquals(now, outputRecord.value.getTimeUtc());
-        assertEquals("UPS", outputRecord.value.getCarrier());
-        assertEquals(List.of("Cosy jumper", "Wooly hat"), outputRecord.value.getManifest());
-        assertEquals("TRACK-ABC123", outputRecord.value.getTrackingId());
-        assertEquals("TRACK-ABC123", outputRecord.value.getTrackingId());
-        assertEquals("TRACK-ABC123", outputRecord.value.getTrackingId());
+            SpecificAvroSerde<logistics_delivered> outputSerde = new SpecificAvroSerde<>();
+            outputSerde.configure(serdeConfig, false); // just for the value
+
+            inputTopic = testDriver.createInputTopic(inputTopicName, Serdes.String().serializer(), inputSerde.serializer());
+            outputTopic = testDriver.createOutputTopic(outputTopicName, Serdes.String().deserializer(), outputSerde.deserializer());
+        }
+
+        @AfterAll
+        static void tearDown() {
+            testDriver.close();
+        }
+
+        @Test
+        @DisplayName("Test a Delivered message with partial content goes through")
+        void testPartialDeliveredMessagePropagates() {
+            GenericRecord inputValue = new GenericData.Record(inputSchema.rawSchema());
+            inputValue.put("state", "Delivered");
+
+            // We expect this to fail because the app tries to read fields out of the
+            // message value that are not present and not defined in the input schema.
+            // This should cause a StreamsException error, caused by an AvroRuntimeException
+            // with a messsage something like:
+            //    Not a valid schema field: time_utc
+
+            Exception exception = assertThrows(StreamsException.class, () -> {
+                inputTopic.pipeInput("key", inputValue);
+            } );
+
+            var cause = exception.getCause();
+            assertEquals(AvroRuntimeException.class, cause.getClass());
+            assertTrue(cause.getMessage().contains("Not a valid schema field"));
+        }
     }
 }
