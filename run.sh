efafd953aebb6b38a12baad03188c17fa1ae3bd1
Tony Ibbs (Tibs) <tony.ibbs@aiven.io>
2026-01-23 15:11:00 +0000

    Add support for the EXACTLY_ONCE value

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@
 #
 # If you give a value for SCHEMA_REGISTRY_USERNAME we'll use it, otherwise
 # we'll use the default value, which is "avnadmin"
-export SCHEMA_REGISTRY_USERNAME=${SCHEMA_REGISTRY_USERNAME:-avnadmin}
+export SCHEMA_REGISTRY_USERNAME=${SCHEMA_REGISTRY_USERNAME:-"avnadmin"}
 #
 # You can also give values for INPUT_TOPIC and OUTPUT_TOPIC, but if
 # they're unset, then the Java application will use an appropriate
@@ -19,8 +19,12 @@ export SCHEMA_REGISTRY_USERNAME=${SCHEMA_REGISTRY_USERNAME:-avnadmin}
 export INPUT_TOPIC=${INPUT_TOPIC:-""}
 export OUTPUT_TOPIC=${OUTPUT_TOPIC:-""}
 #
+# You can also request exactly once semantics by specifying true.
+# The case of the value does not matter. The default is false.
+export EXACTLY_ONCE=${EXACTLY_ONCE:-"false"}
+#
 # We need the name of the app (its class name), but we've got a default
-export APP_NAME=${APP_NAME:-GenericLogApp}
+export APP_NAME=${APP_NAME:-"GenericLogApp"}
 
 echo "APP_NAME is $APP_NAME"
 
@@ -29,14 +33,15 @@ echo "APP_NAME is $APP_NAME"
 echo "RUN THE PROGRAM"
 exec java \
     -cp '$JAVA_HOME/lib/*' \
-    -DKAFKA_SERVICE_URI=$KAFKA_SERVICE_URI                     \
+    -DKAFKA_SERVICE_URI="$KAFKA_SERVICE_URI"                   \
     -DCA_PEM_CONTENTS="$CA_PEM_CONTENTS"		       \
     -DSERVICE_CERT_CONTENTS="$SERVICE_CERT_CONTENTS"           \
     -DSERVICE_KEY_CONTENTS="$SERVICE_KEY_CONTENTS"             \
-    -DSCHEMA_REGISTRY_URL=$SCHEMA_REGISTRY_URL                 \
-    -DSCHEMA_REGISTRY_USERNAME=$SCHEMA_REGISTRY_USERNAME       \
-    -DSCHEMA_REGISTRY_PASSWORD=$SCHEMA_REGISTRY_PASSWORD       \
-    -DINPUT_TOPIC=$INPUT_TOPIC                                 \
-    -DOUTPUT_TOPIC=$OUTPUT_TOPIC                               \
+    -DSCHEMA_REGISTRY_URL="$SCHEMA_REGISTRY_URL"               \
+    -DSCHEMA_REGISTRY_USERNAME="$SCHEMA_REGISTRY_USERNAME"     \
+    -DSCHEMA_REGISTRY_PASSWORD="$SCHEMA_REGISTRY_PASSWORD"     \
+    -DINPUT_TOPIC="$INPUT_TOPIC"                               \
+    -DOUTPUT_TOPIC="$OUTPUT_TOPIC"                             \
+    -DEXACTLY_ONCE="$EXACTLY_ONCE"                             \
     -jar ./${APP_NAME}-uber.jar \
     com.example.${APP_NAME} "$@"
